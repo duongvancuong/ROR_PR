42ce9a4560ba9e2bbf1528585ef45c28c5764e97
duongvancuong <vancuong279dn@gmail.com>
2017-12-26 18:04:51 +0700

    Implement Presenter Pattern

---
+++ app/presenter/base_presenter.rb
@@ -0,0 +1,7 @@
+class BasePresenter < SimpleDelegator
+  attr_reader :model, :view
+  def initialize model, view
+    @model, @view = model, view
+    super @model
+  end
+end
---
+++ app/presenter/product_presenter.rb
@@ -0,0 +1,5 @@
+class ProductPresenter < BasePresenter
+  def product_name
+
+  end
+end
